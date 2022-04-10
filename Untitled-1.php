3e3ebf091c85fcf5561950927c0e1d2061c8c456
jatupon1323 <103355411+jatupon1323@users.noreply.github.com>
2022-04-10 16:26:19 +0700

    Add files via upload

---
+++ Untitled-1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>ifelse2</h1>
+<?php
+$money = 100;
+if ($money <= 500) {
+    echo "Buy a Totoro model";
+} else {
+    echo "Buy a one piece model";
+}
+?>
+</body>
+</html>
